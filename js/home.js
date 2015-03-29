7e906d32af916a75a397c5d2d13c2412d64ec217
kartamyshev <kostya85>
2015-03-29 13:57:33 +0300

    quotation for A.R. book

--- js/home.js
+++ js/home.js
@@ -30,13 +30,16 @@ var App = (function(window, document, undefined) {
 
     return {
         go: function() {
+
             var i, j = this.init;
             for (i in j) {
                 j.hasOwnProperty(i) && j[i]();
             }
         },
         init: {
-
+            saveText: function (text) {
+                text = text || ''
+            }
         }
     };
 
